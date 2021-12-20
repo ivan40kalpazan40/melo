06c69125207c1501932a72de3a410ec81eeceb35
Ivan Skordev <maymunskoa@yahoo.com>
2021-12-20 17:10:53 +0200

    home screen

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,8 +3,87 @@ const Home = () => {
   const { user } = useAuth();
   return (
     <div className='ui container'>
-      <h1>Home</h1>
-      <p>{user ? 'Authenticated' : 'Not Authenticated'}</p>
+      <h1>Welcome to :melo:::</h1>
+      <h3>{user ? `${user.username}'s home screen` : 'Not Authenticated'}</h3>
+
+      {/*  */}
+      {user ? (
+        <div class='ui placeholder segment'>
+          <div class='ui three column stackable center aligned grid'>
+            <div class='middle aligned row'>
+              <div class='column'>
+                <div class='ui icon header'>
+                  <i class='music icon'></i>
+                  See Random Artists
+                </div>
+                <div class='ui black button'>Go To List</div>
+              </div>
+              <div class='column'>
+                <div class='ui icon header'>
+                  <i class='search icon'></i>
+                  Search Artist, Genre or Release
+                </div>
+                <div class='field'>
+                  <div class='ui search'>
+                    <div class='ui icon input'>
+                      <input
+                        class='prompt'
+                        type='text'
+                        placeholder='artist, genres, etc. ...'
+                      />
+                      <i class='search icon'></i>
+                    </div>
+                    <div class='results'></div>
+                  </div>
+                </div>
+              </div>
+
+              <div class='column'>
+                <div class='ui icon header'>
+                  <i class='user plus icon'></i>
+                  Add Friend
+                </div>
+                <div class='ui black button'>Find Friends</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      ) : (
+        <div class='ui placeholder segment'>
+          <div class='ui two column stackable center aligned grid'>
+            <div class='middle aligned row'>
+              <div class='column'>
+                <div class='ui icon header'>
+                  <i class='music icon'></i>
+                  See Random Artists
+                </div>
+                <div class='ui primary button'>Go To List</div>
+              </div>
+              <div class='column'>
+                <div class='ui icon header'>
+                  <i class='search icon'></i>
+                  Search Artist, Genre or Release
+                </div>
+                <div class='field'>
+                  <div class='ui search'>
+                    <div class='ui icon input'>
+                      <input
+                        class='prompt'
+                        type='text'
+                        placeholder='artist, genres, etc. ...'
+                      />
+                      <i class='search icon'></i>
+                    </div>
+                    <div class='results'></div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+
+      {/*  */}
     </div>
   );
 };
